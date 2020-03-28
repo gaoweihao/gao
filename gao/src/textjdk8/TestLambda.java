5a9d0f313ccef1daefab34b23b90aa3281d12b55
gaowh <504837278@QQ.com>
2020-03-28 16:46:22 +0800

    测试

--- gao/src/textjdk8/TestLambda.java
+++ gao/src/textjdk8/TestLambda.java
@@ -3,6 +3,8 @@ package textjdk8;
 public class TestLambda {
     public static void main(String[] args) {
         System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
     }
 }
 
